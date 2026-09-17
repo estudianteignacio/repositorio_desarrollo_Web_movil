3c3248c96d65ec37d8755fceb7180ed655024c17
BerryMaddy <mtfigueroapino@gmail.com>
2026-09-16 21:19:19 -0300

    creacion de fastapi 1 y gateway, y adaptacion de estos a empresa elegida

---
+++ Semana 7/apigateway/fastapi/backend_api.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="Backend API Pasteles",
+    description="API ubicada en localhost enrutada por API gateway",
+
+)
+
+@app.get("/health")
+def health():
+    return {
+        "status": "OK",
+        "service": "Backend API"
+        }
+
+@app.get("/products")
+def products():
+    return {
+        "products": [
+            {"id": 1, "name": "Pastel", "price": 50.0},
+            {"id": 2, "name": "Torta", "price": 50.0},
+            {"id": 3, "name": "Panqueque", "price": 40.0}
+        ]
+    }
+
+@app.get("/orders")
+def orders():
+    return {
+        "orders": [
+            {"id": 1, "status": "paid"},
+            {"id": 2, "status": "pending"},
+            {"id": 3, "status": "paid"}
+        ]
+    }
